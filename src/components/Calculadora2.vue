bed20f24820da320a1058e6a2de0ce34921fe8da
vainawebcpx <vainawebcpx@gmail.com>
2017-12-05 16:39:46 -0200

    calculadora2

--- src/components/Calculadora2.vue
+++ src/components/Calculadora2.vue
@@ -1,25 +1,25 @@
 <template>
-	<div>
-		<h1>Calculadora</h1>
-		<input type="number" v-model.number="resultado">
+	<div id="calc">
+		<h1 class="calc-h1">Calculadora</h1>
+		<input class="input-calc" type="number" v-model="visor">
 		<br>
-		<section>
-			<button v-on:click="number('1')">1</button>
-			<button v-on:click="number('2')">2</button>
-			<button v-on:click="number('3')">3</button>
-			<button v-on:click="number('4')">4</button>
-			<button v-on:click="number('5')">5</button>
-			<button v-on:click="number('6')">6</button>
-			<button v-on:click="number('7')">7</button>
-			<button v-on:click="number('8')">8</button>
-			<button v-on:click="number('9')">9</button>
+		<section class="butao">
+			<button v-on:click="preencher('1')">1</button>
+			<button v-on:click="preencher('2')">2</button>
+			<button v-on:click="preencher('3')">3</button>
+			<button v-on:click="preencher('4')">4</button>
+			<button v-on:click="preencher('5')">5</button>
+			<button v-on:click="preencher('6')">6</button>
+			<button v-on:click="preencher('7')">7</button>
+			<button v-on:click="preencher('8')">8</button>
+			<button v-on:click="preencher('9')">9</button>
 		</section>
-			<section>
-				<button v-on:click="calc('somar')">+</button>
-				<button v-on:click="calc('subtrair')">-</button>
-				<button v-on:click="calc('multiplicar')">*</button>
-				<button v-on:click="calc('dividir')">/</button>
-				<button v-on:click="calc('igual')">=</button>
+			<section class="butao2">
+				<button v-on:click="setOperador('somar')">+</button>
+				<button v-on:click="setOperador('subtrair')">-</button>
+				<button v-on:click="setOperador('multiplicar')">*</button>
+				<button v-on:click="setOperador('dividir')">/</button>
+				<button v-on:click="calc()">=</button>
 				<button v-on:click="zerar()">C</button>
 			</section>
 	</div>
@@ -30,58 +30,65 @@
 		name:'Calculadora2',
 		data(){
 			return{
-				v1:0,
-				v2:0,
-				resultado:null
+				v1:'',
+				v2:'',
+				resultado:'',
+				operador:null,
+				visor:''
 			}
 		},
 		methods:{
-			number(operador){
-				if(operador === '1'){
-					this.resultado = 1
-				} else if(operador === '2'){
-					this.resultado = 2
-				}else if(operador === '3'){
-					this.resultado = 3
-				}else if(operador === '4'){
-					this.resultado = 4
-				}else if(operador === '5'){
-					this.resultado = 5
-				}else if(operador === '6'){
-					this.resultado = 6
-				}else if(operador === '7'){
-					this.resultado = 7
-				}else if(operador === '8'){
-					this.resultado = 8
-				}else if(operador === '9'){
-					this.resultado = 9
+			preencher(value){
+				if (this.operador === null){
+					this.v1 = this.v1 + value
+					this.visor = this.v1
+				} else{
+					this.v2 = this.v2 + value
+					this.visor = this.v2
 				}
 			},
-			calc(operador){
-				if(operador === 'somar'){
-					this.resultado = this.v1 + this.v2
-				}else if(operador === 'subtrair'){
-					this.resultado = this.v1 - this.v2
-				}else if (operador === 'multiplicar'){
-					this.resultado = this.v1 * this.v2
-				}else if(operador === 'dividir'){
-					this.resultado = this.v1 / this.v2
-				}else if(operador === 'igual'){
-					this.resultado =this.v1 = v2
-				} 
-
+			setOperador(operador){
+				this.operador = operador
 			},
-			zera(){
-				this.v1 = 0
-				this.v2 = 0
-				this.resultado = null
+			calc(){
+				if(this.operador === 'somar'){
+					this.resultado = parseInt(this.v1) + parseInt(this.v2)
+				}else if(this.operador==='subtrair'){
+					this.resultado=parseInt(this.v1)-parseInt(this.v2)
+				}else if (this.operador==='multiplicar'){
+					this.resultado=parseInt(this.v1)*parseInt(this.v2)
+				}else if(this.operador==='dividir'){
+					this.resultado=parseInt(this.v1)/parseInt(this.v2)
+				}
+				this.visor = parseInt(this.resultado) 
 			}
-
 		}
-				
-	}
+			
+}
+
 </script>
 
 <style scoped>
-	
+	#calc{
+		display:block;
+		margin:0 auto;
+		padding: 20px;
+		background-color:#f0f;
+		width:300px;
+		height:70vh;
+		border-radius:15px;
+	}
+	.calc-h1{
+		font-family: verdana;
+		font-size:20px;
+		text-align:center;
+		color:#f00;
+		padding-top:80px;
+	}
+	.butao{
+		display:flex;
+		flex-direction:column;
+		padding-top:40px;
+
+	}
 </style>
